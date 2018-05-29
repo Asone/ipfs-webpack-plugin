29093a99efddae0d6d44e7691dccb5fb3e5ca13f
Asone <Asone@akbarworld.info>
2018-05-29 22:24:47 +0200

    WIP : adding another entry point to test multiple entries

--- example/webpack.config.js
+++ example/webpack.config.js
@@ -1,12 +1,18 @@
-const path = require('path')
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin')
 // Replace `../ipfs-webpack-plugin` with `ipfs-webpack-plugin` in your application
 const IPFSWebpackPlugin = require('../ipfs-webpack-plugin')
 
 module.exports = {
-  entry: './index.js',
+  entry: {
+    app: './index.js',
+    vendors: './extend.js'
+  },
   output: {
     path: path.resolve(__dirname, 'dist'),
     filename: '[name].bundle.js?v=[hash:12]'
   },
-  plugins: [new IPFSWebpackPlugin()]
+  plugins: [
+    new HtmlWebpackPlugin(),
+    new IPFSWebpackPlugin()]
 }
